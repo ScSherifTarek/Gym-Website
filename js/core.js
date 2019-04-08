cb06dc3d562284fd636f8eff61a917c69efe789c
ScSherifTarek <sherift1552@gmail.com>
2019-04-08 15:39:04 +0200

    Cross Section Updates

---
+++ js/core.js
@@ -0,0 +1,18 @@
+$(function() {
+    $("#core-section").height($("#core-section").height() - 80);
+  $("#tabs").tabs({
+    show: { effect: "blind", direction: "right", duration: 300 }
+  });
+  $( "#accordion" ).accordion();
+
+  var btn = $('#accordion li a');
+  var wrapper = $('#accordion li');
+
+  $(btn).on('click', function() {
+    $(btn).removeClass('active');
+    $(btn).parent().find('.addon').removeClass('fadein');
+    
+    $(this).addClass('active');
+    $(this).parent().find('.addon').addClass('fadein');
+  });
+});
