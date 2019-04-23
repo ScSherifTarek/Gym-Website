83969ee0defb1f94b38b06c5fca11c26be486e4a
ScSherifTarek <sherift1552@gmail.com>
2019-04-23 22:09:20 +0200

    Solving the lagging issue

--- js/core.js
+++ js/core.js
@@ -1,18 +1,18 @@
 $(function() {
-    $("#core-section").height($("#core-section").height()-50);
-  $("#tabs").tabs({
-    show: { effect: "blind", direction: "right", duration: 300 }
-  });
-  $( "#accordion" ).accordion();
-
-  var btn = $('#accordion li a');
-  var wrapper = $('#accordion li');
-
-  $(btn).on('click', function() {
-    $(btn).removeClass('active');
-    $(btn).parent().find('.addon').removeClass('fadein');
+    var btns = $('#accordion li a');
+    var wrapper = $('#accordion li');
+    $(btns).not('.active').parent().find('.addon').removeClass('fadein').fadeOut(300);
     
-    $(this).addClass('active');
-    $(this).parent().find('.addon').addClass('fadein');
-  });
+    $(btns).on('click', function() {
+        if($(this).hasClass('active'))
+            return;
+        $(btns).removeClass('active');
+        $(btns).parent().find('.addon').removeClass('fadein').slideUp(300);
+        $('div[id^="tabs-"]').fadeOut(300);
+      
+        $(this).addClass('active');
+        $(this).parent().find('.addon').addClass('fadein').slideDown(300);
+        $tab = $(this).attr("targetTab");
+        $("#"+$tab).fadeIn(300);
+    });
 });
