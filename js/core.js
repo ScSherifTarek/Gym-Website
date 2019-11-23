d065bde41255cba9be81683ec992eed7ed5f3654
Sherif Tarek <sherift1552@gmail.com>
2019-11-23 13:10:06 +0200

    Removing the contacts opacity effects from thee phone view

--- js/core.js
+++ js/core.js
@@ -18,75 +18,46 @@ $(function() {
     });
 
 
-    var navWrap = $('#tabs'),
-        nav = $('#browser'),
-        diff = 80,
-        startPosition = nav.offset().top - diff,
-        stopPosition = startPosition + navWrap.outerHeight() - 2 * nav.outerHeight() + diff;
+    if($(window).width()>1000){
+        var navWrap = $('#tabs'),
+            nav = $('#browser'),
+            diff = 80,
+            startPosition = nav.offset().top - diff,
+            stopPosition = startPosition + navWrap.outerHeight() - 2 * nav.outerHeight() + diff;
 
-    var contact = $("#new-contact");
-    contact.mouseenter(function(){
-        $(this).stop();
-        $(this).fadeTo(100,1); 
-    });
-
-    contact.mouseleave(function(){
-        var y = $(document).scrollTop();
-        console.log(y);
-        if(y > startPosition && y < stopPosition)
-        {
+        var contact = $("#new-contact");
+        contact.mouseenter(function(){
             $(this).stop();
-            $(this).fadeTo(200,0.3); 
-        }
-    });
+            $(this).fadeTo(100,1); 
+        });
 
-    $(document).scroll(function () {
-        //stick nav to top of page
-        var y = $(this).scrollTop();
-
-        if (y > startPosition) {
-            nav.addClass('browser-fixed');
-            if (y > stopPosition) {
-                nav.css('top', stopPosition - y + diff);
-               contact.stop().fadeTo(100,1); 
-            } else {
-                nav.css('top', diff);
-                contact.stop().fadeTo(200,0.3)
+        contact.mouseleave(function(){
+            var y = $(document).scrollTop();
+            if(y > startPosition && y < stopPosition)
+            {
+                $(this).stop();
+                $(this).fadeTo(200,0.3); 
             }
-        } else {
-            nav.removeClass('browser-fixed');
-            nav.css('top', 0);
-            contact.stop().fadeTo(100,1); 
-        } 
-    });
+        });
 
-    // const featuresSectionTop = $("#core-section").offset().top;
-    // const diff = $("#browser").offset().top - featuresSectionTop;
-    // let featuresSectionBottom = $("#core-section").offset().top + $("#core-section").height() - diff;
-    // const tabsBottom = $("#tabs").offset().top + $("#tabs").height() ;
-    // featuresSectionBottom += featuresSectionBottom - tabsBottom;
-    // const browserHeight = 400;
-    // $(window).scroll(function(){
-    //     let curScrollVal = $(window).scrollTop();
-    //     let curScrollValDown = curScrollVal + $(window).height();
-    //     let browserBottom = $("#browser").offset().top + browserHeight;
-    //     if( browserBottom < tabsBottom && curScrollVal >= featuresSectionTop  && curScrollValDown < featuresSectionBottom){
-    //         $('#browser').css({'position': 'fixed', 'top': diff+'px', 'bottom':'auto'});
-    //         $('.new-contact').first().fadeTo(200,0.3).mouseenter(function() {
-    //             $(this).fadeTo(200,1);
-    //         });
-    //         $('.new-contact').first().mouseleave(function() {
-    //             $(this).fadeTo(200,0.3);
-    //         })
-    //     }
-    //     else if( curScrollVal < featuresSectionTop ){
-    //         $('#browser').css({'position': 'absolute', 'top': 0, 'bottom': 'auto'});
-    //         $('.new-contact').first().fadeTo(200,1);
-    //     }
-    //     else{
-    //         $('#browser').css({'position': 'absolute', 'top': 'auto', 'bottom':'100px'});
-    //         $('.new-contact').first().fadeTo(200,1);
-    //     }
+        $(document).scroll(function () {
+            //stick nav to top of page
+            var y = $(this).scrollTop();
 
-    // });
+            if (y > startPosition) {
+                nav.addClass('browser-fixed');
+                if (y > stopPosition) {
+                    nav.css('top', stopPosition - y + diff);
+                   contact.stop().fadeTo(100,1); 
+                } else {
+                    nav.css('top', diff);
+                    contact.stop().fadeTo(200,0.3)
+                }
+            } else {
+                nav.removeClass('browser-fixed');
+                nav.css('top', 0);
+                contact.stop().fadeTo(100,1); 
+            } 
+        });
+    }
 });
